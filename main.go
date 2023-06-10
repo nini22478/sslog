ee392274ef9b1188fc763bdf21b53906d35782c1
mxnmac <511541512@qq.com>
2023-06-10 22:56:49 +0800

    按照时间排序

--- main.go
+++ main.go
@@ -4,13 +4,9 @@ import (
 	"jdudp/Console"
 	"jdudp/routers"
 	"jdudp/utils"
-
-	database "jdudp/dbtabases"
 )
 
 func main() {
-	database.InitDB()
-	database.Connection()
 	utils.InitRedisUtil("120.55.54.57", 6379, "zhutao@123")
 	router := routers.InitRouter()
 	// main.go中关闭定时任务
